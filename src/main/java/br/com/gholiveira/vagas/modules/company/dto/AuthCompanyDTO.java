381f1752ad007f101d88b93aa7ca42706bd92373
gholiveira29 <g.h.p.o3615@gmail.com>
2024-05-04 15:46:01 -0300

    criando token de autenticação para company

---
+++ src/main/java/br/com/gholiveira/vagas/modules/company/dto/AuthCompanyDTO.java
@@ -0,0 +1,11 @@
+package br.com.gholiveira.vagas.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDTO {
+    private String password;
+    private String username;
+}
